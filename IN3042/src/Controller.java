9b95c65f164c2b0ceafe1b8897eab383d082e1cf
Laura Hanna-White <laurahannawhite@gmail.com>
2015-03-27 16:57:50 +0000

    +Controller class

--- IN3042/src/Controller.java
+++ IN3042/src/Controller.java
@@ -7,6 +7,24 @@
  *
  * @author Laura
  */
-public class Controller {
+public class Controller implements Runnable{
     
+    @Override
+    public void run(){
+        
+    }
+    
+    public static void main(String args[]) {
+        //Court c = new Court();
+           Gang sharks = new Gang("Sharks");
+           Gang jets = new Gang("Jets");
+           sharks.setOthers(jets);
+           jets.setOthers(sharks);
+           System.out.println("TEST");
+           Thread s = new Thread(sharks);
+           Thread j = new Thread(jets);
+            s.start();
+            j.start();
+    }
+
 }
