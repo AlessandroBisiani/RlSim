1f3cd2f1baab6411a3de11c769c3547ecb2d146e
Laura Hanna-White <laurahannawhite@gmail.com>
2015-03-27 17:05:02 +0000

    Files weren't working, compiled them, should be okay now

---
+++ IN3042/src/in3042/Controller.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in3042;
+
+/**
+ *
+ * @author Laura
+ */
+public class Controller implements Runnable{
+    
+    @Override
+    public void run(){
+        
+    }
+    
+    public static void main(String args[]) {
+        //Court c = new Court();
+           Gang sharks = new Gang("Sharks");
+           Gang jets = new Gang("Jets");
+           sharks.setOthers(jets);
+           jets.setOthers(sharks);
+           System.out.println("TEST");
+           Thread s = new Thread(sharks);
+           Thread j = new Thread(jets);
+            s.start();
+            j.start();
+    }
+}
